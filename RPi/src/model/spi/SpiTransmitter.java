fff4fd306e0cde749c5595feeb3812264aaed6cd
Calioups <38938563+Calioups@users.noreply.github.com>
2019-01-29 18:38:39 +0100

    Update SpiTransmitter.java

--- RPi/src/model/spi/SpiTransmitter.java
+++ RPi/src/model/spi/SpiTransmitter.java
@@ -9,20 +9,22 @@ import model.event.SynthParameterEditListener;
  *
  */
 public class SpiTransmitter implements SynthParameterEditListener<Object> {
-
+	
+	private SpiDevice spi;
 	/**
 	 * Initialize the SPI bus with the correct speed and parameter, using Pi4J library.
 	 */
-	public SpiTransmitter() {
+	public SpiTransmitter() throws IOException {
 		super();
 		// TODO : @loic SPI initialization
-		
+		this.spi = SpiFactory.getInstance(SpiChannel.CS0, 500000);
 	}
 
 	@Override
 	public void synthParameterEdited(SynthParameterEditEvent<Object> e) {
 		// TODO Auto-generated method stub
 		Object o = e.getValue();
+		short data[] = new short[]
 		/*if (o instanceof Double) bla bla bla
 		else if (o instance of Boolean) bla bla bla
 		else bla bla bla*/
